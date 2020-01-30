70f5e4b98f59e0543290c85ae00c6bcc06293cc4
Md Maruf prodhan <59418855+mdmarufprodhan@users.noreply.github.com>
2020-01-31 00:38:09 +0600

    Basic Tuple and List print

---
+++ tuple.py
@@ -0,0 +1,28 @@
+#A tuple is collection which is orderd and unchanged. Allows duplicate numbers
+
+fruits_tuple = ('apple', 'Orange','Mango',)
+print(fruits_tuple)
+
+
+#get single  value
+print(fruits_tuple[1])
+fruits_tuple1 = ('Grape',)
+print(fruits_tuple1)
+
+print(len(fruits_tuple1))
+
+
+#create set
+
+fruit_set = {'Apple','Orange','Mango'}
+#print(fruit_set)
+#Check if in set
+
+print('Apple' in fruit_set)
+
+#Add to set
+#fruit_set.add()
+fruit_set.clear()
+print(fruit_set)
+del fruit_set
+
