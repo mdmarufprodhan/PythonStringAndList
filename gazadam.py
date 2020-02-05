6ae6b434ed598f256725bc176b53f0249de35c75
Md Maruf prodhan <59418855+mdmarufprodhan@users.noreply.github.com>
2020-02-06 01:32:39 +0600

    create my probelm and solution

---
+++ gazadam.py
@@ -0,0 +1,16 @@
+for x in range(1, 25):
+    for y in range(2, 25):
+        for z in range(3,25):
+            print(25)
+       # totallop = -25
+        #totallop+=1
+        #print(totallop)
+        #totalnum = 0
+
+
+
+
+
+
+     #print(y)
+          #print(z)
